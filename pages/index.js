3264557751454afa5ff9b8f08e6aaa3fe797de8d
AgonIdrizi <agon.88@hotmail.com>
2021-02-02 13:28:43 +0100

    add Google login, add redirect links

--- pages/index.js
+++ pages/index.js
@@ -23,25 +23,35 @@ export default function Home() {
         <title>Fast Feedback</title>
       </Head>
       <Logo />
+      <p className="text-gray-600 p-3 mb-6 mt-2 max-w-xs ">
+        <span className="font-bold inline">Fast feedback</span>
+        {` It's the easiest way to add comments or reviews to your static site. It's still a work-in-progress, but you can try it out by logging in. `}
+      </p>
       {auth.user ? (
         <>
-          <p className="text-gray-600 max-w-xs ">
-            <span className="font-bold inline">Fast feedback</span>
-            {` It's the easiest way to add comments or reviews to your static site. It's still a work-in-progress, but you can try it out by logging in. `}
-          </p>
           <Link href="/dashboard" passHref>
-            <Button btnClassType={BUTTON_CLASS_TYPES.secondaryButton}>
-              View Dashboard
-            </Button>
+            <a>
+              <Button btnClassType={BUTTON_CLASS_TYPES.secondaryButton}>
+                View Dashboard
+              </Button>
+            </a>
           </Link>
         </>
       ) : (
-        <Button
-          btnClassType={BUTTON_CLASS_TYPES.secondaryButton}
-          onClick={(e) => auth.signinWithGithub()}
-        >
-          Sign In
-        </Button>
+        <div>
+          <Button
+            btnClassType={BUTTON_CLASS_TYPES.primaryButton}
+            onClick={(e) => auth.signinWithGithub()}
+          >
+            Sign in with Github
+          </Button>
+          <Button
+            btnClassType={BUTTON_CLASS_TYPES.secondaryButton}
+            onClick={(e) => auth.signinWithGoogle()}
+          >
+            Sign in with Google
+          </Button>
+        </div>
       )}
     </main>
   );
